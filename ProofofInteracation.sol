1c25e97ed13ba92f1de8a2b75ff71abe57783b0e
Sankalp17305 <153196953+Sankalp17305@users.noreply.github.com>
2025-02-21 14:05:36 +0530

    Update ProofofInteracation.sol

--- ProofofInteracation.sol
+++ ProofofInteracation.sol
@@ -1,1 +1,27 @@
+pragma solidity ^0.8.0;
 
+contract InteractionLogger {
+    struct Interaction {
+        address user;
+        uint256 timestamp;
+    }
+
+    Interaction[] public interactions;
+
+    event InteractionLogged(address indexed user, uint256 timestamp);
+
+    function logInteraction() public {
+        interactions.push(Interaction(msg.sender, block.timestamp));
+        emit InteractionLogged(msg.sender, block.timestamp);
+    }
+
+    function getInteraction(uint256 index) public view returns (address, uint256) {
+        require(index < interactions.length, "Index out of bounds");
+        Interaction memory interaction = interactions[index];
+        return (interaction.user, interaction.timestamp);
+    }
+
+    function getInteractionCount() public view returns (uint256) {
+        return interactions.length;
+    }
+}
